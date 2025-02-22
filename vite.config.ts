9851cdcb048064ad52e6c54cf2536542c965b446
DiogoBrazil <diogoifroads@gmail.com>
2025-02-21 20:55:06 -0400

    feat: implements docker file and compose to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite acesso externo no Docker
+    proxy: {
+      '/api': {
+        target: 'http://api:5000', // Nome do serviço no Docker
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
